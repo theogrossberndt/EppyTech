e013499949d74d9716670a60e568e3c34cf001b0
theogrossberndt <theogrossberndt@gmail.com>
2024-08-09 20:36:23 -0400

    Fixed sizing of form submitted cover, cleaned up form logic, and changed catcha to visible

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -17,17 +17,20 @@ import ReCAPTCHA from 'react-google-recaptcha';
 
 import {ContextProvider} from "@/app/appProvider.tsx";
 
+const phoneRegex: RegExp = new RegExp("^(\\+?1?)\\s*([\\(-]?\\s*\\d{3}\\s*\\)?\\s*-?)\\s*\\d{3}\\s*-?\\s*\\d{4}(\\s*[xX\\s]\\s*\\d+)?$");
+
 export default function ContactPage(){
 	const formRef = useRef<HTMLFormElement>(null);
-	const [defaultWidth, setDefaultWidth] = useState<number>(0);
+	const [textHeight, setTextHeight] = useState<number>(0);
 	const [errors, setErrors] = useState<{[key: string]: Array<string>}>({});
 	// 0: fillable, 1: loading (sending email), 2: submitted
 	const [formState, setFormState] = useState<number>(0);
+
 	const nameToReadable: {[key: string]: string} = {fname: "first name", lname: "last name", email: "email", phone: "phone number", message: "message"};
 
 	const context = useContext(ContextProvider);
 
-	const captchaRef = useRef();
+	const captchaRef = useRef<HTMLElement>(null);
 
 	const onSubmit = (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
@@ -35,38 +38,49 @@ export default function ContactPage(){
 			return;
 
 		const formData: FormData = new FormData(event.currentTarget);
+		const formObj = {};
 
 		let newErrors: {[key: string]: Array<string>} = {}
 		/////////////////// Validation
 		if (true)
 			Object.keys(nameToReadable).forEach((key: string) => {
 				const val = formData.get(key) as string | null;
+				formObj[key] = val;
 				if (val == null || val.trim().length == 0){
 					if (!newErrors[key])
 						newErrors[key] = [];
 					newErrors[key].push("The " + nameToReadable[key] + " field is required.");
 				}
+				if (key == 'phone' && !newErrors[key]){
+					const isValid: boolean = phoneRegex.test(val);
+					if (!isValid){
+						newErrors[key] = ["The " + nameToReadable[key] + " field must be a valid 10 digit phone number."];
+					}
+				}
 			});
+		const token = captchaRef.current?.getValue();
+		if (token == null || token == "")
+			newErrors["form"] = ["Please complete the ReCAPTCHA."];
 		setErrors(newErrors);
+//		console.log(formObj);
 		if (Object.keys(newErrors).length == 0){
 			setFormState(1);
-			console.log(captchaRef.current?.executeAsync);
-			captchaRef.current?.executeAsync().then((token) => {
-				console.log(formRef.current, token);
-				wait()
-//				emailjs.sendForm("service_6omc8df","template_2e6g0zv", formRef.current, {publicKey: "lDCeJnFwBEYnQGVHD"}, )
-					.then(
-						() => setFormState(2),
-						(error) => {
-							console.log(error);
-							setErrors({form: ["The form could not be submitted. Please email support@eppytech.com with your information and mention this error.", error]});
-							setFormState(0);
-						});
-			}, (error) => {
-				console.log(error);
-				setErrors({form: ["There was an error with reCAPTCHA. Please email support@eppytech.com with your information and mention this error.", error]});
-				setFormState(0);
-			});
+//			console.log(captchaRef.current?.executeAsync);
+			captchaRef.current?.reset();
+//			console.log(formRef.current, token);
+			formObj['g-recaptcha-response'] = token;
+//			wait()
+			emailjs.send("service_6omc8df","template_2e6g0zv", formObj, {publicKey: "lDCeJnFwBEYnQGVHD"})
+				.then(
+					() => setFormState(2),
+					(error) => {
+						console.log(error);
+						if (error.text === 'reCAPTCHA: The g-recaptcha-response parameter not found')
+							setErrors({form: ["The form could not be submitted due to a reCAPTCHA issue. Please retry the reCAPTCHA and resubmit, or email support@eppytech.com with your information and mention this error if the problem persists."]});
+						else
+							setErrors({form: ["The form could not be submitted. Please email support@eppytech.com with your information and mention this error.", error.text]});
+						setFormState(0);
+					});
 		}
 	}
 
@@ -76,23 +90,25 @@ export default function ContactPage(){
 		}, 2000));
 	}
 
+	const removeKeyFromErrors = (toRemove: string) => {
+		if (errors[toRemove])
+			setErrors((oldErrors: {[key: string]: Array<string>}) => {
+				let newErrors: {[key: string]: Array<string>} = {}
+				Object.keys(oldErrors).forEach((targetKey: string) => {
+					if (toRemove !== targetKey)
+						newErrors[targetKey] = oldErrors[targetKey];
+				});
+				return newErrors;
+			});
+	}
+
 	const formField = (label: string, type: string, key: string): ReactElement => {
 		return (
 			<div className={[styles.block, errors[key] ? styles.error : ""].join(" ")}>
 				<label htmlFor={key}>{label}</label>
-				<input type={type} id={key} name={key} onInput={() => {
-					if (errors[key])
-						setErrors((oldErrors: {[key: string]: Array<string>}) => {
-							let newErrors: {[key: string]: Array<string>} = {}
-							Object.keys(oldErrors).forEach((nKey: string) => {
-								if (key !== nKey)
-									newErrors[nKey] = oldErrors[nKey];
-							});
-							return newErrors;
-						});
-				}}/>
+				<input type={type} id={key} name={key} onInput={() => removeKeyFromErrors(key)}/>
 				<AnimatePresence>
-					{errors[key] && errors[key].map((err, idx) => (
+					{errors[key] && errors[key].map((err, idx) => (err instanceof String || typeof err === 'string') && (
 						<motion.div key={idx} animate={{height: 'auto'}} initial={{height: 0}} exit={{height: 0}} style={{overflow: 'hidden'}}>
 							<div className={styles.errorMsg}>
 								<FontAwesomeIcon icon={faCircleExclamation} style={{width: '1rem', height: '1rem', paddingRight: '0.5rem'}} ariahidden="true"/>
@@ -120,7 +136,7 @@ export default function ContactPage(){
 						<form className={styles.form} onSubmit={onSubmit} ref={formRef} autoComplete="on">
 							<fieldset disabled={formState != 0}>
 								<AnimatePresence>
-									{errors['form'] && errors['form'].map((err, idx) => (
+									{errors['form'] && errors['form'].map((err, idx) => (err instanceof String || typeof err === 'string') && (
 										<motion.div key={idx} animate={{height: 'auto'}} initial={{height: 0}} exit={{height: 0}} style={{overflow: 'hidden'}}>
 											<div className={styles.errorMsg}>
 												<FontAwesomeIcon icon={faCircleExclamation} style={{width: '1rem', height: '1rem', paddingRight: '0.5rem'}}/>
@@ -152,39 +168,41 @@ export default function ContactPage(){
 									(required)
 								</div>
 								{formField("Please provide a brief description of your concern or need.", "text", "message")}
+									<ReCAPTCHA
+										ref={captchaRef}
+										sitekey="6LcfmxwqAAAAAOnvSZd-1-wNNw3vPGslu1OdLHal"
+										badge="inline"
+										style={{marginTop: '1rem'}}
+										onChange={(value: string) => removeKeyFromErrors('form')}
+									/>
 								<div style={{display: 'flex'}}>
-									<motion.div className={styles.button} layout style={{borderRadius: 50, padding: 0, overflow: 'hidden'}} transition={{duration: 0.5}} disabled={formState != 0}>
-										{formState == 0 ? (
-											<motion.input layout className={styles.button} style={{margin: 0, boxShadow: 'none', fontFamily: 'inherit'}}
-												type="submit" value="SUBMIT" disabled={formState != 0} key="0"/>
-										) : (
+									<motion.div className={styles.button} layout style={{borderRadius: 50, padding: 0, overflow: 'hidden', ...(formState != 0 && {backgroundColor: '#35598e'})}} transition={{duration: 0.5}}>
+										{formState == 1 ? (
 											<motion.div layout style={{padding: '1rem', display: 'flex', flexDirection: 'row'}}>
 												<div ref={el => {
 													if (el)
-														setDefaultWidth(el.offsetHeight);
+														setTextHeight(el.offsetHeight);
 												}}>SUBMITTING</div>
-												<motion.div className={styles.loader} animate={{rotate: 360}} transition={{repeat: Infinity, duration: 1}} style={{height: defaultWidth, width: defaultWidth}}>
+
+												<motion.div className={styles.loader} animate={{rotate: 360}} transition={{repeat: Infinity, duration: 1}} style={{height: textHeight, width: textHeight}}>
 												</motion.div>
 											</motion.div>
+										) : (
+											<motion.input layout className={styles.button} style={{margin: 0, boxShadow: 'none', fontFamily: 'inherit'}}
+												type="submit" value="SUBMIT" disabled={formState != 0}/>
 										)}
 									</motion.div>
 								</div>
-									<ReCAPTCHA
-										ref={captchaRef}
-										size="invisible"
-										sitekey="6LcfmxwqAAAAAOnvSZd-1-wNNw3vPGslu1OdLHal"
-										badge="inline"
-									/>
 							</fieldset>
 						</form>
 						<AnimatePresence>
 							{formState == 2 && (
-								<motion.div style={{top: '0.5rem', bottom: '0.5rem', left: '1rem', right: '1rem', position: 'absolute', backgroundColor: '#fff'}}
-								animate={{opacity: '100%'}} initial={{opacity: '0%'}} exit={{opacity: '0%'}} transition={{duration: 1, ease: [0.65, 0, 0.35, 1]}}
-								onAnimationComplete={def => {
-									if (formRef.current)
-										formRef.current.reset();
-								}}>
+								<motion.div className={styles.overlayWrapper} animate={{opacity: '100%'}} initial={{opacity: '0%'}} exit={{opacity: '0%'}} transition={{duration: 1, ease: [0.65, 0, 0.35, 1]}}
+									onAnimationComplete={def => {
+										if (formRef.current)
+											formRef.current.reset();
+									}
+								}>
 									<RoundedButton onClick={() => setFormState(0)} arialabel="Return">
 										<FontAwesomeIcon icon={faChevronLeft} style={{width: '1rem', height: '1rem'}} ariahidden="true"/>
 									</RoundedButton>

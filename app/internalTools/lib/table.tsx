6672e302b8e2f994d395468b77ac156ae96a9636
theogrossberndt <theogrossberndt@gmail.com>
2025-01-29 14:22:36 -0500

    Using new table library and its way better.  Reimplemented with the new lib and added some custom dragging logic to preferentially shrink certain columns. Still need to add preferential expansion

--- app/internalTools/lib/table.tsx
+++ app/internalTools/lib/table.tsx
@@ -1,224 +1,144 @@
-import { useState, useMemo, memo } from 'react';
+import { useRef, useState, useMemo, memo, useCallback } from 'react';
 import styles from './table.module.css';
+import { AgGridReact } from 'ag-grid-react';
+import { AllCommunityModule, ModuleRegistry, themeQuartz } from 'ag-grid-community';
 
-import {
-  useReactTable,
-  getCoreRowModel,
-  ColumnDef,
-  flexRender,
-  Table,
-} from '@tanstack/react-table'
+// TODO: Pair down modules
+ModuleRegistry.registerModules([AllCommunityModule]);
 
-type Product = {
-	name: string
-	location: string
-	tags: string
-	count: number
+const themeBase = {
+	fontFamily: 'monospace',
+	fontSize: '1rem'
+};
+
+const myTheme = themeQuartz.withParams(themeBase);
+
+const makeRandomItem = () => {
+	return {product: 'Product', location: 'Here', tags: 'Printer', stock: 1};
 }
-
-const defaultColumns: ColumnDef<Product>[] = [
-	{
-		accessorKey: 'name',
-		cell: (info) => info.getValue(),
-		header: 'Product Name',
-	},
-	{
-		accessorKey: 'location',
-		cell: (info) => info.getValue(),
-		header: 'Location',
-	},
-	{
-		accessorKey: 'tags',
-		cell: (info) => info.getValue(),
-		header: 'Tags',
-	},
-	{
-		accessorKey: 'count',
-		cell: (info) => info.getValue(),
-		header: 'Stock',
-	},
-];
 
 export default function InventoryTable(){
-	const [data, _setData] = useState(() => makeData(25))
-	const [columns] = useState<typeof defaultColumns>(() => [
-		...defaultColumns,
-	])
+	const charWidth = useCharacterWidth();
+	const padding = 30;
+	const fiveChar = charWidth*6+padding;
+	const eightChar = charWidth*8+padding;
+	console.log("charWidth", charWidth);
 
-	const table = useReactTable({
-		data,
-		columns,
-		columnResizeMode: 'onChange',
-		getCoreRowModel: getCoreRowModel(),
-		debugTable: true,
-		debugHeaders: true,
-		debugColumns: true,
-		enableColumnResizing: true,
-	})
+	const [ rowData, setRowData ] = useState(Array(10).fill().map(d => makeRandomItem()));
+	const [ colDefs, setColDefs ] = useState([
+		{field: 'product', },
+		{field: 'location', minWidth: eightChar, initialWidth: eightChar},
+		{field: 'tags', minWidth: eightChar*2},
+		{field: 'stock', lockPosition: 'right', resizable: false, minWidth: fiveChar, initialWidth: fiveChar}
+	]);
 
-	const columnSizeVars = useMemo(() => {
-		const headers = table.getFlatHeaders()
-		const colSizes: { [key: string]: number } = {}
-		for (let i = 0; i < headers.length; i++) {
-			const header = headers[i]!
-			colSizes[`--header-${header.id}-size`] = header.getSize()
-			colSizes[`--col-${header.column.id}-size`] = header.column.getSize()
+	const rowClassRules = {
+		[styles.rowNormalEven]: params => params.data.stock > 0 && params.rowIndex % 2 === 0,
+		[styles.rowNormalOdd]: params => params.data.stock > 0 && params.rowIndex % 2 === 1,
+		[styles.rowRedEven]: params => params.data.stock === 0 && params.rowIndex % 2 === 0,
+		[styles.rowRedOdd]: params => params.data.stock === 0 && params.rowIndex % 2 === 1,
+	}
+
+	const gridOptions = {
+		suppressScrollOnNewData: true,
+		autoSizeStrategy: {
+			type: 'fitGridWidth',
+			columnLimits: [
+				{colId: 'stock', minWidth: fiveChar},
+				{colId: 'location', minWidth: eightChar}
+			]
+		},
+		defaultColDef: {
+			flex: 1,
 		}
-		return colSizes
-	}, [table.getState().columnSizingInfo, table.getState().columnSizing])
+	}
 
-	//demo purposes
-	const [enableMemo, setEnableMemo] = useState(true);
+	// Attempts to resize a column as much as possible
+	// Returns the amount of overage that still needs to be compensated for
+	// IE if this column could be reduced by 10px but 40px need to be reduced, this function returns 30
+	const resizeColIfPossible = (api: GridApi<TData>, colName: string, over: number): boolean => {
+		const col = api.getColumn(colName);
+		if (!col)
+			return over;
+		const currentWidth = col.getActualWidth();
+		const minWidth = col.getMinWidth();
+		if (currentWidth > minWidth){
+			const reduceBy = Math.min(currentWidth-minWidth, over);
+			console.log("Resizing ", colName, " by ", reduceBy, " px to compensate");
+			api.setColumnWidths([{key: colName, newWidth: currentWidth-reduceBy, finished: false}]);
+			return over-reduceBy;
+		}
+		return over;
+	}
 
-	if (false)
+	const onResize = useCallback((event: ColumnResizedEvent<TData>) => {
+		if (!event.api || !event.finished)
+			return;
+
+		// So long as nothing catestrophic has happened to reduce the grid size below the container width
+		// the width should always be equal to the horizontal pixel range
+		const range = event.api.getHorizontalPixelRange();
+		if (!range)
+			return;
+		const width = range.right - range.left;
+
+		const stockCol = event.api.getColumn('stock');
+		if (!stockCol)
+			return;
+		// If any resize causes the stock column to start past the width - stock min width, we will be out of frame (breaking)
+		// If its not a breaking resize event, theres nothing to do
+		if (stockCol.getLeft() <= width-stockCol.getMinWidth()){
+//			console.log(stockCol.getLeft(), width-stockCol.getMinWidth(), width, stockCol.getMinWidth(), stockCol.getColId());
+			console.log("Good resize");
+			return;
+		}
+
+		// If we get to here, its a breaking resize that needs to be fixed
+		// Calculate how bad it is to start
+		console.log("BAD RESIZE!!!!", event.column);
+		let over = stockCol.getLeft() - (width - stockCol.getMinWidth());
+		console.log("Over by:", over, "px");
+
+		// The stock column should be resized to it's minimum automatically, so no need to check it
+		// Then, column resizing goes (in order): location, tags, product
+		const keys = ['location', 'tags', 'product'];
+		keys.some((key: string): boolean => {
+			if (event.column && event.column.colId !== key)
+				over = resizeColIfPossible(event.api, key, over);
+			return over <= 0;
+		});
+	}, [])
+
 	return (
-		<div style={{overflowX: 'hidden', overflowY: 'scroll', width: '100%', display: 'block'}}>
-			<table {...{
-				className: styles.divTable,
-				style: {
-					...columnSizeVars, //Define column sizes on the <table> element
-					overflowY: 'scroll'
-				}
-			}}>
-				<thead>
-					{table.getHeaderGroups().map(headerGroup => (
-						<tr key={headerGroup.id}>
-							{headerGroup.headers.map(header => (
-								<th {...{
-									key: header.id,
-									className: styles.th,
-									style: {
-										width: `calc(var(--header-${header?.id}-size) * 1px)`,
-//										width: header.getSize(),
-										position: 'relative'
-									},
-									colSpan: header.colSpan
-								}}>
-									{header.isPlaceholder ? null : flexRender(
-										header.column.columnDef.header,
-										header.getContext()
-									)}
-									<div {...{
-										onDoubleClick: () => header.column.resetSize(),
-										onMouseDown: header.getResizeHandler(),
-										onTouchStart: header.getResizeHandler(),
-										className: [styles.resizer, header.column.getIsResizing() ? 'isResizing' : ''].join(' '),
-									}}/>
-								</th>
-							))}
-						</tr>
-					))}
-				</thead>
-				{table.getState().columnSizingInfo.isResizingColumn && enableMemo ? (
-					<MemoizedTableBody table={table} />
-				) : (
-					<TableBody table={table} />
-				)}
-			</table>
+		<div className={styles.divTable}>
+			<AgGridReact rowData={rowData} columnDefs={colDefs} rowClassRules={rowClassRules} theme={myTheme} gridOptions={gridOptions} defaultColDef={gridOptions.defaultColDef} onColumnResized={onResize}/>
 		</div>
 	);
+};
 
-//	if (false)
-	return (
-		<div style={{overflow: 'hidden', width: '100%', display: 'flex', flexDirection: 'column'}}>
-			<div>
-			<table {...{
-				className: styles.divTable,
-				style: {
-					...columnSizeVars, //Define column sizes on the <table> element
-				}
-			}}>
-				<thead>
-					{table.getHeaderGroups().map(headerGroup => (
-						<tr key={headerGroup.id}>
-							{headerGroup.headers.map(header => (
-								<th {...{
-									key: header.id,
-									className: styles.th,
-									style: {
-										width: `calc(var(--header-${header?.id}-size) * 1px)`,
-//										width: header.getSize(),
-										position: 'relative'
-									},
-									colSpan: header.colSpan
-								}}>
-									{header.isPlaceholder ? null : flexRender(
-										header.column.columnDef.header,
-										header.getContext()
-									)}
-									<div {...{
-										onDoubleClick: () => header.column.resetSize(),
-										onMouseDown: header.getResizeHandler(),
-										onTouchStart: header.getResizeHandler(),
-										className: [styles.resizer, header.column.getIsResizing() ? 'isResizing' : ''].join(' '),
-									}}/>
-								</th>
-							))}
-						</tr>
-					))}
-				</thead>
-			</table>
-			</div>
+const useCharacterWidth = () => {
+  const didCompute = useRef(false);
+  const widthRef = useRef(0);
 
-		<div style={{overflowX: 'hidden', overflowY: 'scroll', width: '100%', display: 'block', flexGrow: 0}}>
-			<table {...{
-				className: styles.divTable,
-				style: {
-					...columnSizeVars, //Define column sizes on the <table> element
-					overflowY: 'scroll'
-				}
-			}}>
-				{table.getState().columnSizingInfo.isResizingColumn && enableMemo ? (
-					<MemoizedTableBody table={table} />
-				) : (
-					<TableBody table={table} />
-				)}
-			</table>
-		</div>
-		</div>
-	);
+  if (didCompute.current) return widthRef.current;
 
-}
+  // Creating invisible container
+  const outer = document.createElement('div');
+  outer.style.fontFamily = themeBase.fontFamily;
+  outer.style.fontSize = themeBase.fontSize;
+  outer.style.display = 'inline-block';
+  outer.style.visibility = 'hidden';
+  outer.innerHTML = 'a';
+  document.body.appendChild(outer);
 
-//un-memoized normal table body component - see memoized version below
-function TableBody({ table }: { table: Table<Person> }) {
-	return (
-		<tbody className={styles.tbody}>
-			{table.getRowModel().rows.map(row => (
-				<tr {...{
-					key: row.id,
-					className: styles.tr,
-				}}>
-					{row.getVisibleCells().map(cell => {
-						return (
-							<td {...{
-								key: cell.id,
-								className: styles.td,
-								style: {
-									width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
-								},
-							}}>
-								{cell.renderValue<any>()}
-							</td>
-						)
-					})}
-				</tr>
-			))}
-		</tbody>
-	);
-}
+  // Calculating difference between container's full width and the child width
+  const charWidth = outer.offsetWidth;
 
-//special memoized wrapper for our table body that we will use during column resizing
-export const MemoizedTableBody = memo(
-  TableBody,
-  (prev, next) => prev.table.options.data === next.table.options.data
-) as typeof TableBody
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
 
-function makeData(len){
-	return Array(len).fill().map((d) => ({
-		name: 'Product name is really long',
-		location: 'Here',
-		tags: 'None',
-		count: 10,
-	}));
-}
+  didCompute.current = true;
+  widthRef.current = charWidth;
+
+  return charWidth;
+};
